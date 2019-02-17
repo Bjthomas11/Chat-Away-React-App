3b085f9ddfc8cfc406399af58fb83a1c164aa23a
Brian Thomas <Brianthomas571@gmail.com>
2019-02-17 00:10:42 -0600

    added styling and normalize

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MessageList from "./components/MessageList";
 import SendMessageForm from "./components/SendMessageForm";
 import RoomList from "./components/RoomList";
 import NewRoomForm from "./components/NewRoomForm";
-import { tokenUrl, instanceLocator } from "./config";
+import { tokenURL, instanceLocator } from "./config";
 
 class App extends React.Component {
   constructor() {
@@ -26,7 +26,7 @@ class App extends React.Component {
       instanceLocator,
       userId: "bjthomas11",
       tokenProvider: new Chatkit.TokenProvider({
-        url: tokenUrl
+        url: tokenURL
       })
     });
 
