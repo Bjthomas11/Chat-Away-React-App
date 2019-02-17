6e36af87127351963e20e5a96580ec22dbd9baff
Brian Thomas <Brianthomas571@gmail.com>
2019-02-16 23:56:30 -0600

    adding components and config and app state

---
+++ config.js
@@ -0,0 +1,6 @@
+const tokenURL =
+  "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/d1e86472-6825-4e58-b9a0-35d9059d46c5/token";
+
+const instanceLocator = "v1:us1:d1e86472-6825-4e58-b9a0-35d9059d46c5";
+
+export { tokenURL, instanceLocator };
